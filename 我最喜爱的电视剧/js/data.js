bc6f5d12fed8c3d307ef975ab14157620fee4109
wujin <2359066246@qq.com~>
2023-08-09 11:45:21 +0800

    把ajax  请求加到了  vue中 但是还有问题  待修复

--- 我最喜爱的电视剧/js/data.js
+++ 我最喜爱的电视剧/js/data.js
@@ -2,25 +2,25 @@
 
 
 
-$.ajax({
-    type: 'get', // 请求的方式，例如 GET 或 POST
-    url: 'http://baike.baidu.com/api/openapi/BaikeLemmaCardApi?', // 请求的 URL 地址
-    data: {
-        scope: 103,
-        format: "json",
-        appid: 379020,
-        bk_key: "武林外传",
-        bk_length: 600
-    }, // 这次请求要携带的数据
+// $.ajax({
+//     type: 'get', // 请求的方式，例如 GET 或 POST
+//     url: 'http://baike.baidu.com/api/openapi/BaikeLemmaCardApi?', // 请求的 URL 地址
+//     data: {
+//         scope: 103,
+//         format: "json",
+//         appid: 379020,
+//         bk_key: "武林外传",
+//         bk_length: 600
+//     }, // 这次请求要携带的数据
 
-    dataType: "jsonp",
-    success: function(res) {
-        console.log(res)
-        console.log(res.key)
-    }, // 请求成功之后的回调函数
-    error: function() {}, //请求失败
-    complete: function() {} //请求完成
-})
+//     dataType: "jsonp",
+//     success: function(res) {
+//         console.log(res)
+//         console.log(res.key)
+//     }, // 请求成功之后的回调函数
+//     error: function() {}, //请求失败
+//     complete: function() {} //请求完成
+// })
 
 $.getJSON("./js/index.json", function(data) {
     // console.log(data)
