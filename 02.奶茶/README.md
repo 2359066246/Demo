10d84b2d3d1d27e60389e444a560d2296e01799c
wujin <2359066246@qq.com~>
2023-08-19 22:25:39 +0800

    第二个基本完成 但是还有好几个bug 有js的 和css的

---
+++ 02.奶茶/README.md
@@ -0,0 +1,54 @@
+# 制作点单系统
+
+## 问题记录
+8.14
+目前遇到两个问题  
+1、 复选框的值不知道怎么获取(8.19已解决)
+2、在表格的这个地方  flex布局有问题
+
+
+8.19
+1. 不记得 怎么再简写的 怎么显示这个了
+```js
+开始这个地方取值时候 用错了  
+试了一下 {},{{}},{$}  正确写法 应该是${}
+`您点的奶茶口味是:${form.nc_name.value};
+您点的数量: ${form.number.value}; 
+```
+2. ` `  这个写法 好像是只支持 模板
+``` js
+
+` 这个写法好像是调用不了 函数的` 
+```
+3. 
+
+
+## 知识点
+
+1. 复选框值选中
+``` js
+function getCheckbox(element) {
+    for (let i = 0; i < element.length; i++) {
+        var str = "";
+        // 我感觉 我这地方 写的很垃圾 ，不知道有没有其他写法
+        for (let i = 0; i < element.length; i++) {
+            if (element[i].checked == true) {
+                str += element[i].value + ",";
+            }
+        }
+    }
+    return str
+}
+```
+
+
+2. 阻止submi默认提交事件
+``` js
+// 阻止submit 按钮的默认提交事件 调用函数返回 false
+var submit = document.getElementById("submit");
+submit.onclick = function() {
+    alert("表单已经提交");
+    return false;
+
+}
+```
---
+++ 02.奶茶/js/index.js
@@ -0,0 +1,76 @@
+var form = document.getElementById("form");
+var address = document.getElementById("address");
+var notes = document.getElementById("notes");
+// 奶茶口味：
+// form.nc_name.value
+// 数量：
+// form.number.value
+// 杯型：
+// form.bx.value
+// 甜度：
+// form.sugar.value
+// 免费小料：
+// getCheckbox(form.xl)
+// 加价小料：
+// getCheckbox(form.jlxl)
+// 是否去冰：
+// form.qb.value
+// 是否去茶底：
+// form.cd.value
+// 地址
+// address.value
+// 手机号
+// form.phone.value
+// 送达时间
+// form.time.value
+// 备注：
+// notes.value
+// 支付方式
+// form.money.value
+// 封装个复选框的函数
+
+function getCheckbox(element) {
+    for (let i = 0; i < element.length; i++) {
+        var str = "";
+        // 我感觉 我这地方 写的很垃圾 ，不知道有没有其他写法
+        for (let i = 0; i < element.length; i++) {
+            if (i == element.length - 1 || i == 0) {
+                if (element[i].checked == true) {
+                    str += element[i].value;
+                }
+            } else if (element[i].checked == true) {
+                str += element[i].value + ",";
+            }
+        }
+    }
+    return str
+}
+
+
+// 提交函数
+var submit = document.getElementById("submit");
+submit.onclick = function() {
+    // alert("表单已经提交");
+    var form_xl = getCheckbox(form.xl)
+    var form_jlxl = getCheckbox(form.jlxl)
+    let str =
+        `【您的订单已经生成】
+        ------------------------
+        奶茶口味：${form.nc_name.value}
+        数量：${form.number.value}
+        杯型：${form.bx.value}
+        甜度：${form.sugar.value}
+        免费小料：${form_xl}
+        加价小料：${form_jlxl}
+        是否加冰：${form.qb.value}
+        是否去茶底：${form.cd.value}
+        地址：${address.value}
+        手机号：${form.phone.value}
+        期待送达时间：${form.time.value}
+        备注：${notes.valuet}
+        支付方式：${form.money.value}
+    `;
+    alert(str)
+    return false;
+
+}
